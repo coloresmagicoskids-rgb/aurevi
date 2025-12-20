597fe3248fbbc8561613fcc2f114327ab709a7b3
Carlos J. Polanco <coloresmagicoskids@gmail.com>
2025-12-20 14:08:05 -0600

    chore: ensure WorldProvider import matches filename case

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+// src/main.jsx
 import { WorldProvider } from "./worlds/WorldContext.jsx";
 import "./index.css";
 
